75d67b1039b91d6380c932cb1ae10cb832aa8423
David Ayala and Josh Huffman <david@>
2016-11-03 10:41:58 -0700

    display total price in shopping cart

--- app/components/checkout-component.js
+++ app/components/checkout-component.js
@@ -1,5 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  cart: Ember.inject.service()
+
+  itemCount: Ember.computed('cart', function(){
+    var currentCart = this.get('cart');
+    return currentCart.length;
+  }),
+
+  totalPrice: Ember.computed('cart', function(){
+    var total = 0;
+    var currentCart = this.get('cart');
+    // console.log(currentCart[0].get('price'));
+    currentCart.forEach(function(unit){
+      console.log(unit.get('price'));
+      total += parseFloat(unit.get('price'));
+    })
+    return total;
+  })
+
+
 });
--- app/routes/checkout.js
+++ app/routes/checkout.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  cart: Ember.inject.service(),
+
+  model() {
+    var cart = [];
+    this.get('cart').unitArray.forEach(function(item){
+      cart.push(item);
+    });
+    return cart;
+  }
 });
